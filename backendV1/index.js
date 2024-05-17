0d2c0a6635ba375067f788bcdbbff10e097f8e88
junemel610 <meljune610@gmail.com>
2024-05-18 02:14:28 +0800

    Update index.js

--- backendV1/index.js
+++ backendV1/index.js
@@ -8,6 +8,12 @@ const Router = require('./routes/QuickGasRoute');
 const app = express();
 const port = 8000;
 
+// Middleware to log the IP address
+app.use((req, res, next) => {
+    console.log(`Request from IP: ${req.ip}`);
+    next();
+  });
+  
 app.use(bodyParser.json());
 app.use(cors({
   origin: '*', // Replace with another frontend URL
